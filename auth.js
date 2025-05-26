6e73ab4c59ff4e1f1b8a0de72c21997ee243b394
GabrielZippys <id1366351@outlook.com>
2025-05-25 21:53:27 -0300

    25/05

--- auth.js
+++ auth.js
@@ -2,7 +2,6 @@ function checkAuth() {
     // Verifica se o token de login NÃO existe no localStorage
     if (!localStorage.getItem('googleToken')) {
         // Se não existir, avisa o usuário e redireciona para o login
-        alert("Acesso negado. Por favor, faça o login.");
         window.location.href = 'login.html';
     }
 }
